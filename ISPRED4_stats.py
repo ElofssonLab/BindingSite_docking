7b843d757a1ab080262033b161bc00e744ad0032
gabrielepozzati <gabrielepozzati@hotmail.it>
2020-12-10 13:43:57 +0100

    plot update

--- ISPRED4_stats.py
+++ ISPRED4_stats.py
@@ -4,11 +4,15 @@ import sys
 import math
 import random
 import argparse
+import statistics
 from Bio.PDB import *
 from Bio.PDB.DSSP import DSSP
 import multiprocessing as mp
+import numpy as np
+
 from sklearn.metrics import auc
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 18})
 import seaborn as sb
 import pandas as pd
 
@@ -60,7 +64,7 @@ def auc_stats(scores):
 
     TPRs = [0.0]
     FPRs = [0.0]
-    PPVs = [0.0]
+    PPVs = []
     TP = FP = FN = TN = 0
     for el in scores: 
         if el[2] == 1.0: FN += 1
@@ -81,7 +85,16 @@ def auc_stats(scores):
         else: 
             TN -= 1
             FP += 1
+
+        if PPVs == []: PPVs.append(TP/(TP+FP))
         prev = el[1]
+
+    if  (TP != 0 or FN != 0)\
+    and (FP != 0 or TN != 0)\
+    and (TP != 0 or FP != 0):
+        TPRs.append(TP/(TP+FN))
+        FPRs.append(FP/(FP+TN))
+        PPVs.append(TP/(TP+FP))
 
     return TPRs, FPRs, PPVs
 
@@ -94,30 +107,111 @@ if __name__ == "__main__":
     parser.add_argument("-s", type=str, help="structures files folder")
     ns = parser.parse_args()
 
-    scores = []
-    p = PDBParser(QUIET=True)
+    count = 0
+    metrics = {}
+    allscores = []
     codelist = [line.rstrip() for line in open(ns.l)]
+    p = PDBParser(QUIET=True)
     for code in codelist:
+        splitscores = []
+        pdbcode = code.rstrip('_u12')
         isp = open(ns.p+code+'.ispred4.tsv')
         ist = open(ns.t+code+'.ispred4.tsv')
         pdb = p.get_structure('', ns.s+code+'.pdb')
         for c in pdb[0]: chain = c.get_id() 
         dssp = DSSP(pdb[0], ns.s+code+'.pdb')
         rasa = [line[3] for line in dssp]
-        scores += [[lp.split('\t')[0].split('_')[0], float(lp.split('\t')[-1]),\
-                    float(lt.split('\t')[-1])] for lp, lt, r in zip(isp,ist,rasa)\
-                   if r >= 0.2]
 
-    sorted_scores = mergesort_pred(scores)
-    TPRs, FPRs, PPVs = auc_stats(sorted_scores)
+        splitscores = [[lp.split('\t')[0].split('_')[0], float(lp.split('\t')[-1]),\
+                        float(lt.split('\t')[-1])] for lp, lt, r in zip(isp,ist,rasa)\
+                       if r >= 0.2]
 
-    sb.lineplot(FPRs, TPRs)
-    plt.show()
+        allscores += splitscores[:]
+        sortedl = mergesort_pred(splitscores)
+        TPRs, FPRs, PPVs = auc_stats(sortedl)
+        if len(TPRs) > 2:
+            PRauc = auc(TPRs, PPVs)
+            ROCauc = auc(FPRs, TPRs)
+            topPPV = [sum([1 for el in sortedl[:int(lbl)] if el[-1] == 1.0])/float(lbl)\
+                      for lbl in ['5', '10', '20']]
+            if not pdbcode in metrics: metrics[pdbcode] = [PRauc, ROCauc, TPRs, FPRs, PPVs] + topPPV
+            elif metrics[pdbcode][0] > PRauc: 
+                metrics[pdbcode] = [PRauc, ROCauc, TPRs, FPRs, PPVs] + topPPV
 
-    sb.lineplot(TPRs, PPVs)
-    plt.show()
+    sortedl = mergesort_pred(allscores)
+    TPRs, FPRs, PPVs = auc_stats(sortedl)
 
+    df = pd.DataFrame(metrics).T
+    best20 = df.sort_values(1, ascending=False)[:10]
+    worst20 = df.sort_values(1, ascending=True)[:10]
+    b20 = list(best20.index)
+    w20 = list(worst20.index)
 
+    fs = 14
+    bins = np.arange(0, 1.1, 0.1) 
+    ##### all ROC curves
+    fig, axes = plt.subplots(1, 2, figsize=(25,10))
+    sb.lineplot(FPRs, TPRs, ci=None, linewidth=1.2, color='purple', ax=axes[0])
+    for key in b20: sb.lineplot(metrics[key][3], metrics[key][2], ci=None, linewidth=0.6, color='blue', ax=axes[0])
+    for key in w20: sb.lineplot(metrics[key][3], metrics[key][2], ci=None, linewidth=0.6, color='orange', ax=axes[0])
+    axes[0].set_xlabel("1 - Specificity")
+    axes[0].set_ylabel("Recall")
+    axes[0].set_xlim(0,1)
+    axes[0].set_ylim(0,1)
 
+    rocs = [metrics[key][1] for key in metrics]
+    sb.distplot(rocs, kde=False, bins=bins, ax=axes[1])
+    mean = statistics.mean(rocs)
+    median = statistics.median(rocs)
+    #axes[1].vlines(mean, 0, 50)
+    axes[1].vlines(median, 0, 70)
+    axes[1].set_xlabel("ROC curve AUCs")
+    axes[1].set_xlim(0,1)
+    axes[1].set_ylim(0,70)
+    fig.savefig('allROCs.png')
+
+    ##### all PR curves
+
+    best20 = df.sort_values(0, ascending=False)[:10]
+    worst20 = df.sort_values(0, ascending=True)[:10]
+    b20 = list(best20.index)
+    w20 = list(worst20.index)
+
+    fig, axes = plt.subplots(1, 2, figsize=(25,10)) 
+    sb.lineplot(TPRs, PPVs, ci=None, linewidth=1.2, color='purple', ax=axes[0])
+    for key in b20: sb.lineplot(metrics[key][2], metrics[key][4], ci=None, linewidth=0.6, color='blue', ax=axes[0])
+    for key in w20: sb.lineplot(metrics[key][2], metrics[key][4], ci=None, linewidth=0.6, color='orange', ax=axes[0])
+    axes[0].set_xlabel("Recall")
+    axes[0].set_ylabel("Precision")
+    axes[0].set_xlim(0,1)
+    axes[0].set_ylim(0,1)
+
+    prs = [metrics[key][0] for key in metrics]
+    sb.distplot(prs, kde=False, bins=bins, ax=axes[1])
+    mean = statistics.mean(prs)
+    median = statistics.median(prs)
+    #axes[1].vlines(mean, 0, 50)
+    axes[1].vlines(median, 0, 100)
+    axes[1].set_xlabel("PR curve AUCs")
+    axes[1].set_xlim(0,1)
+    axes[1].set_ylim(0,100)
+    fig.savefig('allPRs.png')
+
+#    ##### top PPVs
+#    fig, axes = plt.subplots(1, 3, sharey=True, figsize=(25,10))
+#    top5 = [ metrics[key][5] for key in metrics ]
+#    top10 = [ metrics[key][6] for key in metrics ]
+#    top20 = [ metrics[key][7] for key in metrics ]
+#    sb.distplot(top5, kde=False, ax=axes[0])
+#    sb.distplot(top10, kde=False, ax=axes[1])
+#    sb.distplot(top20, kde=False, ax=axes[2])
+#    axes[0].set_xlabel('Top5')
+#    axes[0].set_xlim(0,1)
+#    axes[1].set_xlabel('Top10')
+#    axes[1].set_xlim(0,1)
+#    axes[2].set_xlabel('Top20')
+#    axes[2].set_xlim(0,1)
+#    fig.text(0.513, 0.02, 'Precision', ha='center')
+#    fig.savefig('topPPVs.png')
 
 
