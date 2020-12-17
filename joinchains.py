448f944b405200baeed99316b517ba301956d7b6
gabrielepozzati <gabrielepozzati@hotmail.it>
2020-12-17 14:42:01 +0100

    join chain

--- joinchains.py
+++ joinchains.py
@@ -15,47 +15,40 @@ three2one = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D',
              'THR':'T','TRP':'W','TYR':'Y','VAL':'V',
              'MSE':'M'}
 
-def rewrite(infileu, infileb, chain, outlistu, outlistb, newpos):
-    diff = newpos
+def match_alignment(infile, aligned, chain, newpos):
+    outlist = []
     prev_orig = ''
+    diff = newpos+1
+    for line in open(infile):
+        if line.startswith('ATOM'):
+            if prev_orig != line[22:27]:
+                newpos+=1
+                while three2one[line[17:20]] != aligned[newpos-diff]: newpos+=1
+            renumb = ' '*(4-len(str(newpos)))+str(newpos)+' '
+            outline = line[:21]+chain+renumb+line[27:].rstrip()
+            outlist.append(outline+'\n')
+        prev_orig = line[22:27]
+    outlist.append('TER\n')
+    return newpos, outlist
+
+def renumber(infileu, infileb, chain, firstpos):
     stru = p.get_structure('', infileu)
     strb = p.get_structure('', infileb)
 
-    ##### match chains 
-    for chain in stru[0]: uchain = chain
-    for chain in strb[0]: bchain = chain
+    ##### align chains 
+    for BIOchain in stru[0]: uchain = BIOchain
+    for BIOchain in strb[0]: bchain = BIOchain
     useq = ''.join([three2one[res.get_resname()] for res in uchain])
     bseq = ''.join([three2one[res.get_resname()] for res in bchain])
-    alignments = pairwise2.align.globalds(useq, bseq, blosum62, -10, -0.5)
+    alignments = pairwise2.align.globalds(useq, bseq, blosum62, -2, -0.5)
     ualigned = alignments[0][0]
     baligned = alignments[0][1]
 
-    ##### renumber unbound structure
-    outlistu = []
-    for line in open(infileu):
-        if line.startswith('ATOM'):
-            if prev_orig != '' and prev_orig != line[22:27]:
-                while three2one[line[17:20]] != ualigned[newpos-diff]: newpos+=1
-            renumb = ' '*(4-len(str(newpos)))+str(newpos)+' '
-            outline = line[:21]+chain+renumb+line[27:].rstrip()
-            outlistu.append(outline+'\n')
-        prev_orig = line[22:27]
-    outlistu.append('TER\n')
-
-    ##### renumber bound structure
-    newpos = diff
-    outlistb = []
-    for line in open(infileb):
-        if line.startswith('ATOM'):
-            if prev_orig != '' and prev_orig != line[22:27]: 
-                while three2one[line[17:20]] != baligned[newpos-diff]: newpos+=1
-            renumb = ' '*(4-len(str(newpos)))+str(newpos)+' '
-            outline = line[:21]+chain+renumb+line[27:].rstrip()
-            outlistb.append(outline+'\n')
-        prev_orig = line[22:27]
-    outlistb.append('TER\n')
-
-    return newpos, outlistu, outlistb
+    ##### renumber
+    lastposu, outlistu = match_alignment(infileu, ualigned, chain, firstpos)
+    lastposb, outlistb = match_alignment(infileb, baligned, chain, firstpos)
+    lastpos = max(lastposu, lastposb)
+    return lastpos, outlistu, outlistb
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Analyze ISPRED4 predictions")
@@ -71,6 +64,7 @@ if __name__ == "__main__":
 
     for code in open(ns.c):
         code = code.rstrip()
+        print (code)
         pathu1 = ns.i+'/'+code+'_u1.pdb'
         pathu2 = ns.i+'/'+code+'_u2.pdb'
         pathb1 = ns.i+'/'+code+'_b1.pdb'
@@ -83,14 +77,14 @@ if __name__ == "__main__":
         outb2 = open(ns.o+'/str_b/'+code+'_b2.pdb','w')
         outb = open(ns.o+'/str_b/'+code+'.pdb','w')
     
-        new, linesu1, linesb1 = rewrite(pathu1, pathb1, 'A', [], [], 0)
-        new, linesu2, linesb2 = rewrite(pathu2, pathb2, 'B', [], [], new)
+        new, linesu1, linesb1 = renumber(pathu1, pathb1, 'A', 0)
+        new, linesu2, linesb2 = renumber(pathu2, pathb2, 'B', new)
    
         for line in linesu1: outu1.write(line) 
         outu1.write('END')
         outu1.close()
 
-        for line in linesu1: outu2.write(line)
+        for line in linesu2: outu2.write(line)
         outu2.write('END')
         outu2.close()
 
@@ -102,7 +96,7 @@ if __name__ == "__main__":
         outb1.write('END')
         outb1.close()
 
-        for line in linesb1: outb2.write(line)
+        for line in linesb2: outb2.write(line)
         outb2.write('END')
         outb2.close()
 
