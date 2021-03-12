b44ad042c87c6626295c2785ee3d431e5a2c296e
gabrielepozzati <gabrielepozzati@hotmail.it>
2021-03-12 02:08:29 +0100

    fixed formatter

--- format_dataset.py
+++ format_dataset.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3:q
-from Bio.PDB import *
-from Bio.PDB.DSSP import DSSP
 import argparse
 import random
 import sys
+
+from Bio.PDB import *
+from Bio import pairwise2
+from Bio.Align import substitution_matrices
+from Bio.PDB.Selection import unfold_entities
+from Bio.PDB.NeighborSearch import NeighborSearch
 
 three2one = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D',
              'CYS':'C','GLN':'Q','GLU':'E','GLY':'G',
@@ -12,40 +16,84 @@ three2one = {'ALA':'A','ARG':'R','ASN':'N','ASP':'D',
              'THR':'T','TRP':'W','TYR':'Y','VAL':'V',
              'MSE':'M','GLX':'Q'}
 
-def get_struct_data(path):
-    struct = p.get_structure('', path)
-    dssp = DSSP(struct[0], path)
-    return struct, dssp
+def extract_structure(spath, apath):
+    seq = ''
+    str_map = {}
+    struc = p.get_structure('', spath)
+    rasas = [float(line.split()[5])/100 for line in open(apath) 
+             if line.startswith('RES') and line.split()[2] == chain]
 
-def join_str_dssp(struc, dssp):
-    joined = []
-    for residue in struc[chain]:
-        resnumb = residue.get_id()[1]
-        try: rasa = round(dssp[(chain, resnumb)][3], 3)
-        except: rasa = 0.0
-        if rasa == 'NA': rasa = 0.0
-        joined.append([residue, rasa])
-    return joined
+    for n, res in enumerate(struc[0][chain]):
+        resname = three2one[res.get_resname()]
+        str_map[n+1] = [res, rasas[n]]
+        seq += resname
+    
+    #print (spath, len(str_map.keys()), len(rasas))
+    #print (''.join([three2one[line.split()[1]] for line in open(apath) if line.startswith('RES') and line.split()[2] == chain]))
+    #print (seq)
+
+    assert len(rasas) == len(str_map.keys()),\
+           'Structure and rasa have different lengths!'
+    
+    return str_map, seq
 
 def interface_extract():
-    posc = 0
-    interface = []
-    jointu = join_str_dssp(stru[0], dsspu)
-    jointc = join_str_dssp(strc[0], dsspc)
-    for posu in range(len(jointu)):
-        resu = jointu[posu][0]
-        rasau = jointu[posu][1]
-        rasac = jointu[posc][1]
-        resnumbu = jointu[posu][0].get_id()[1]
-        resnumbc = jointc[posc][0].get_id()[1]
-        if resnumbu < resnumbc: posc += 1
-        if rasac < rasau*0.99: interface.append([resu, rasau, 1.0])
-        else: interface.append([resu, rasau, 0.0])
-        posc += 1
-    return interface
+    ##### setup neighbour search in the interaction partner
+    strc = p.get_structure('', pathc)
+    ns = NeighborSearch(unfold_entities(strc[0][oppchain], 'A'))
+    
+    ##### load structural data and re-align sequences
+    str_mapu, sequ = extract_structure(pathu, pathua)
+    str_mapc, seqc = extract_structure(pathc, pathca)
+    alignments = pairwise2.align.globalxx(sequ, seqc)
+    
+    str_map = {}
+    posb = posc = 1
+    alignedu = alignments[0][0]
+    alignedc = alignments[0][1]
+    print ('u', alignedu)
+    print ('c', alignedc)
+    ##### annotate unbound data in str_map, appending to each 
+    ##### residue info about interaction
+    for b, c in zip(alignedu, alignedc):
+        if posb > len(str_mapu.keys()): break
+        ##### compute if res has atoms within 8A from opposite chain
+        for atom in str_mapu[posb][0]:
+            atom = ns.search(atom.get_coord(), 8)
+            if atom != []: break
 
-def randomize_interfaces(surf, tpr, ppv):
-    randomized = [[residue[1], residue[2]] for residue in surf]
+        ##### if there is a match or mismatch
+        if b != '-' and c != '-':
+            ##### mark as binding site if rasa in unbound chain increases 
+            ##### at least of 10% respective to complex AND if any residue
+            ##### atom is within 8A from the interacting chain
+            if str_mapu[posb][1] > str_mapc[posc][1]*1.1\
+            and atom != []: str_map[posb] = str_mapu[posb]+[1.0]
+            else: str_map[posb] = str_mapu[posb]+[0.0]
+            posb += 1
+            posc += 1
+
+        ##### if there is a gap in the complex
+        elif b != '-':
+            ##### mark as binding site if any residue atom is within
+            ##### 8A from the interacting chain
+            if atom != []: str_map[posb] = str_mapu[posb]+[1.0]
+            else: str_map[posb] = str_mapu[posb]+[0.0]
+            posb += 1
+
+        ##### skip positions gapped in the unbound
+        elif c != '-': 
+            posc += 1
+    
+    ##### convert residue object to one letter aminoacid code
+    for key in str_map: 
+        resname = three2one[str_map[key][0].get_resname()]
+        str_map[key][0] = resname
+
+    return str_map, sequ
+
+def randomize_interfaces(tpr, ppv):
+    randomized = [[intmap[pos][1], intmap[pos][2]] for pos in keylist]
 
     ##### compute number of real binding site (BS) residues
     positives = 0
@@ -84,84 +132,80 @@ def randomize_interfaces(surf, tpr, ppv):
     for p, (acc, score) in enumerate(randomized):
         if score == 0.25: randomized[p][1] = 1.0
         if score == 0.75: randomized[p][1] = 0.0
-        
+    
+    randomized = {pos:randomized[idx] for idx, pos in enumerate(keylist)}
     return randomized
 
-def match_pred(pred, real, predseq, realseq):
-    if len(pred) > len(real):
-        substr = realseq[:5]
-        idx = predseq.find(substr)
-        pred = pred[idx:]
-        pred = pred[:len(real)]
-        return pred, real
-    
-    if len(pred) < len(real):
-        substr = predseq[:5]
-        idx = realseq.find(substr)
-        for n in range(idx): pred = [0.0] + pred
-        while len(pred) < len(real): pred.append(0.0)
-        return pred, real
+def format_ISPRED4():
+    seq = ''
+    predmap = {}
+    for n, line in enumerate(open(isp)):
+        predmap[n+1] = [line.split()[3], line.split()[-1]]
+        seq += line.split()[3]
+    return predmap, seq
 
-    return pred, real
-
-def format_ISPRED4(interface, realseq):
-    pred = [line.split()[-1] for line in open(ns.isp)]
-    predseq = ''.join([line.split()[3] for line in open(ns.isp)])
-    if len(pred) != len(interface):
-        pred, interface = match_pred(pred, interface, predseq, realseq) 
-    if len(pred) != len(interface):
-        print ('RealLen:\t', len(interface))
-        print (realseq)
-        print ('ISPLen:\t', len(pred))
-        print (predseq)
-        sys.exit()
-    return pred
-
-def format_SPPIder(interface, realseq):
-    count = 0
-    pred = []
-    predseq = ''
+def format_SPPIder():
+    seq = ''
+    count = 1
+    predmap = {}
     previous = ''
-    for line in open(ns.spp):
-            if not line.startswith('ATOM'): continue
-            resnum = line[22:26]
-            residue = line[17:20]
-            score = round(float(line[61:66])/100, 2)
-            if resnum != previous:
-                pred.append(score)
-                predseq += three2one[residue]
-                previous = resnum
-                count += 1
-    if len(pred) != len(interface):
-        pred, interface = match_pred(pred, interface, predseq, realseq)
-    if len(pred) != len(interface):
-        print ('RealLen:\t', len(interface))
-        print (realseq)
-        print ('SPPILen:\t', len(pred))
-        print (predseq)
-        sys.exit()
-    return pred
+    for line in open(spp):
+        if not line.startswith('ATOM'): continue
+        resnum = str(int(line[22:26]))
+        resname = three2one[line[17:20]]
+        score = round(float(line[61:66])/100, 2)
+        if resnum != previous:
+            predmap[count] = [resname, score]
+            previous = resnum
+            seq += resname
+            count += 1
+    return predmap, seq
 
-def format_dynJET2(interface, realseq):
-    #pred = [line.split()[11] for line in open(ns.dyn) if not line.startswith('AA')]
-    pred = [max(float(line.split()[12]), float(line.split()[13]), float(line.split()[14]))\
-            if line.split()[15] == 1.0 else 0.0 for line in open(ns.dyn) if not line.startswith('AA')]
-    predseq = ''.join([three2one[line.split()[0]] for line in open(ns.dyn) if not line.startswith('AA')])
-    if len(pred) != len(interface):
-        pred, interface = match_pred(pred, interface, predseq, realseq)
-    if len(pred) != len(interface):
-        print ('RealLen:\t', len(interface))
-        print (realseq)
-        print ('dynLen:\t', len(pred))
-        print (predseq)
-        sys.exit()
-    return pred
+def format_dynJET2():
+    seq = ''
+    count = 1
+    predmap = {}
+    for line in open(dyn):
+        if not line.startswith('AA'):
+            score = max(float(line.split()[12]), 
+                        float(line.split()[13]), 
+                        float(line.split()[14]))
+            resname = three2one[line.split()[0]]
+            if line.split()[15] == '1.0':
+                predmap[count] = [resname, score]
+            else: 
+                predmap[count] = [resname, 0.0]
+            seq += resname
+            count += 1
+    return predmap, seq
+
+def align_predictions(pseq, pdic):
+    alignments = pairwise2.align.globalxx(realseq, pseq)
+
+    posr = posp = 1
+    alignedr = alignments[0][0]
+    alignedp = alignments[0][1]
+    for r, p in zip(alignedr, alignedp):
+        ##### if there is a match or mismatch
+        if r != '-' and p != '-':
+            if r != p: print ('Mismatch!', r, p)
+            intmap[posr].append(pdic[posp][1])
+            posr += 1
+            posp += 1
+        ##### if there is a gap in the prediction
+        elif r != '-':
+            intmap[posr].append(0.0)
+            posr += 1
+        ##### skip positions gapped in unbound sequence
+        elif p != '-':
+            posp += 1
+
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description = 'Converts PDB complex in ISPRED4 format')
-    p.add_argument('-b', required= True, help='bound structure')
-    p.add_argument('-u', required= True, help='unbound structure')
-    p.add_argument('-c', required= True, help='complex structure')
+    p.add_argument('-c', required= True, help='code')
+    p.add_argument('-s', required= True, help='structure path')
+    p.add_argument('-r', required= True, help='naccess rasa path')
     p.add_argument('-isp', required= True, help='ISPRED4 binding site prediction')
     p.add_argument('-spp', required= True, help='SPPIder binding site prediction')
     p.add_argument('-dyn', required= True, help='dynJET2 binding site prediction')
@@ -170,39 +214,48 @@ if __name__ == "__main__":
 
     random.seed(42)
     p = PDBParser(QUIET=True)
-    strb, dsspb = get_struct_data(ns.b)
-    stru, dsspu = get_struct_data(ns.u)
-    strc, dsspc = get_struct_data(ns.c)
-    for c in strb[0]: chain = c.get_id()
-    positions = [res.get_id()[1] for res in strb[0][chain]]
-    print ('######################', ns.u)
+    pathu = ns.s+ns.c
+    pathc = ns.s+ns.c[:-7]+'_b.pdb'
+    pathua = ns.r+ns.c[:-4]+'.rsa'
+    pathca = ns.r+ns.c[:-7]+'_b.rsa'
+    isp = ns.isp+ns.c[:-4]+'.ispred4.tsv'
+    dyn = ns.dyn+ns.c[:-4]+'_jet.res'
+    spp = ns.spp+ns.c
 
+    if '_u1' in ns.c: 
+        chain = 'A'
+        oppchain = 'B'
+    else: 
+        chain = 'B'
+        oppchain = 'A'
+
+    print ('####################', ns.c[:-4])
     ##### binding sites ground truth
-    outfile = open(ns.o, 'w')
-    interface = interface_extract()
-    realseq = ''.join([three2one[res[0].get_resname()]\
-                       for res in interface]) 
-
+    intmap, realseq = interface_extract()
+    keylist = intmap.keys()
+    
     ##### ground truth controlled randomization
     for tpr in [0.25, 0.5]:
         for ppv in [0.5, 0.75]:
-            rand = randomize_interfaces(interface, tpr, ppv)
-            for n, (acc, lbl) in enumerate(rand):
-                interface[n].append(lbl)
+            rand = randomize_interfaces(tpr, ppv)
+            for key in keylist:
+                intmap[key].append(rand[key][1])
 
-    pred = format_ISPRED4(interface, realseq)
-    for n, score in enumerate(pred): interface[n].append(score)
+    ##### mapping predictions to unbound sequence 
+    predisp, seqisp = format_ISPRED4()
+    align_predictions(seqisp, predisp)
+
+    predspp, seqspp = format_SPPIder()
+    align_predictions(seqspp, predspp)
     
-    pred = format_SPPIder(interface, realseq)
-    for n, score in enumerate(pred): interface[n].append(score)
-    
-    pred = format_dynJET2(interface, realseq)
-    for n, score in enumerate(pred): interface[n].append(score)
+    preddyn, seqdyn = format_dynJET2()
+    align_predictions(seqdyn, preddyn)
 
     ##### write out
-    for residue in interface: 
-        for n, val in enumerate(residue[1:]):
-            if n != len(residue)-2: 
+    outfile = open(ns.o+ns.c[:-4]+'.pred', 'w')
+    for pos in keylist:
+        for n, val in enumerate(intmap[pos][1:]):
+            if n != len(intmap[pos][1:])-1: 
                 outfile.write(str(round(float(val), 2))+'\t')
             else: 
                 outfile.write(str(round(float(val), 2))+'\n')
