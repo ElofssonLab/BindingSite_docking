fa68b3e359ea80e76fc510beb42548d631b85b98
gabrielepozzati <gabrielepozzati@hotmail.it>
2021-03-17 18:26:51 +0100

    bash runner

--- src/run_gramm.sh
+++ src/run_gramm.sh
@@ -1,30 +1,21 @@
-#!/bin/bash -x
-#SBATCH -A SNIC2020-5-300
-#SBATCH --output=/proj/nobackup/snic2019-35-62/gpozzati/job_out/gr-%A_%a.out
-#SBATCH --error=/proj/nobackup/snic2019-35-62/gpozzati/job_err/gr-%A_%a.err
-#SBATCH --array=1-223
-#SBATCH -c 7
-#SBATCH -t 01:00:00
-
 list=$1
-data=$2
-out=$3
-col=$4
-offset=$5
+out=$2
+col=$3
 
-pos=$(($SLURM_ARRAY_TASK_ID + $offset))
-id=`tail -n+$pos $list | head -n 1`
-echo 'Processing ' $id '...'
-
-cd $data
-ml singularity/3.5.3
+folder=`pwd`
 
 if [[ ! -f $out ]]
 then
     mkdir $out
 fi
 
-
-singularity exec --bind /proj/nobackup/snic2019-35-62/gpozzati/:/proj/nobackup/snic2019-35-62/gpozzati/ --nv /proj/nobackup/snic2019-35-62/gpozzati/sif/trfull.sif python /proj/nobackup/snic2019-35-62/gpozzati/BindingSite_docking_master/protocol_gramm.py -s1 processed_b4/$id'_u1.pdb' -s2 processed_b4/$id'_u2.pdb' -o $out/$id -g gramm-res/$id'_u1-'$id'_u2.res' -i1 formatted_labels/$id'_u1.site' -i2 formatted_labels/$id'_u2.site' -c $col
-
-for n in {1..10}; do cat processed_b4/$id'_u1.pdb' | grep -v 'END' > $out/$id'_'$n'-tmp'; echo '' >> $out/$id'_'$n'-tmp';  cat $out/$id'_'$n'.pdb' >> $out/$id'_'$n'-tmp'; mv $out/$id'_'$n'-tmp' $out/$id'_'$n'.pdb'; done
+while read id; do
+    echo 'Processing ' $id '...'
+    singularity exec --bind $folder:$folder --nv $folder/BSenv.sif python $folder/src/protocol_gramm.py -s1 $folder/data/processed_b4/$id'_u1.pdb' -s2 $folder/data/processed_b4/$id'_u2.pdb' -o $folder/results/$out/$id -g $folder/data/gramm-res/$id'_u1-'$id'_u2.res' -i1 $folder/data/formatted_labels/$id'_u1.site' -i2 $folder/data/formatted_labels/$id'_u2.site' -c $col;
+    for n in {1..10}; do 
+	cat $folder/data/processed_b4/$id'_u1.pdb' | grep -v 'END' > $folder/results/$out/$id'_'$n'-tmp'; 
+	echo '' >> $folder/results/$out/$id'_'$n'-tmp';  
+	cat $folder/results/$out/$id'_'$n'.pdb' >> $folder/results/$out/$id'_'$n'-tmp'; 
+	mv $folder/results/$out/$id'_'$n'-tmp' $folder/results/$out/$id'_'$n'.pdb'; 
+    done;
+done<$list
