c5a36d25bb0fcb5d782fa7fed704c6906d07f29e
gabrielepozzati <gabrielepozzati@hotmail.it>
2021-03-18 10:48:49 +0100

    core usage

--- src/run_gramm.sh
+++ src/run_gramm.sh
@@ -1,7 +1,7 @@
 list=$1
 out=$2
 col=$3
-
+cores=$4
 folder=`pwd`
 
 if [[ ! -f $out ]]
@@ -11,7 +11,7 @@ fi
 
 while read id; do
     echo 'Processing ' $id '...'
-    singularity exec --bind $folder:$folder --nv $folder/BSenv.sif python $folder/src/protocol_gramm.py -s1 $folder/data/processed_b4/$id'_u1.pdb' -s2 $folder/data/processed_b4/$id'_u2.pdb' -o $folder/results/$out/$id -g $folder/data/gramm-res/$id'_u1-'$id'_u2.res' -i1 $folder/data/formatted_labels/$id'_u1.site' -i2 $folder/data/formatted_labels/$id'_u2.site' -c $col;
+    singularity exec --bind $folder:$folder --nv $folder/BSenv.sif python $folder/src/protocol_gramm.py -s1 $folder/data/processed_b4/$id'_u1.pdb' -s2 $folder/data/processed_b4/$id'_u2.pdb' -o $folder/results/$out/$id -g $folder/data/gramm-res/$id'_u1-'$id'_u2.res' -i1 $folder/data/formatted_labels/$id'_u1.site' -i2 $folder/data/formatted_labels/$id'_u2.site' -c $col -n $cores;
     for n in {1..10}; do 
 	cat $folder/data/processed_b4/$id'_u1.pdb' | grep -v 'END' > $folder/results/$out/$id'_'$n'-tmp'; 
 	echo '' >> $folder/results/$out/$id'_'$n'-tmp';  
